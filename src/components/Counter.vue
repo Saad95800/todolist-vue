2476d6ac2b446fbfab2a6983327492364b8c4aec
Saad <rs.mailpro@gmail.com>
2023-10-05 15:26:40 +0200

    first commit

---
+++ src/components/Counter.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  title: String,
+})
+
+const count = ref(0); // Variable réactive (ou un state)
+const active = ref(true); 
+const items = ref([{ message: 'Foo' }, { message: 'Bar' }, { message: 'toto' }]);
+const item = ref({ message: 'Foo' });
+const {message} = item;
+const visible = ref(true);
+</script>
+
+<template>
+  <h1>{{ title }}</h1>
+    <ul>
+      <li v-for="item in items">
+        {{ item.message }}
+      </li>      
+    </ul>
+
+{{ message }}
+  <div class="card">
+    <button v-if="visible" v-show="true" v-once type="button" @click="count++">count is {{ count }}</button>
+    <p>{{ count }}</p>
+  </div>
+</template>
